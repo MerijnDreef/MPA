f7849c0ce6c9f185449db42d719a77127d86ab62
Merijn <99054535@mydavinci.nl>
2021-12-01 12:12:30 +0100

    have done very little due to other stuff taking up the time
    only things I added was some small texts not of importance

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -5,6 +5,7 @@
 
 <body>
     <a href="/">Send me back</a>
+    <p>Hey dude/dudette you mind logging in?</p>
     <form method='POST'>
         <label for='email'>Email</label><input name='email'>
         <label for='password'>Password</label><input name='password'>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,6 +5,7 @@
 
 <body>
     <a href="/">Send me back</a>
+    <p>Want an account? Go ahead, if you dare</p>
     <form method='POST'>
         <label for='name'>Name</label><input name='name'>
         <label for='email'>Email</label><input name='email'>
